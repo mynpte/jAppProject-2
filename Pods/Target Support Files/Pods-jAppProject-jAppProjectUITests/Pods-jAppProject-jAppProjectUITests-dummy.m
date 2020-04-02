64018eab515068b451d5756c5bf42b4574bffffd
mynpte <mymintmy6@gmail.com>
2020-04-02 14:56:22 +0700

    hello2

---
+++ Pods/Target Support Files/Pods-jAppProject-jAppProjectUITests/Pods-jAppProject-jAppProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jAppProject_jAppProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_jAppProject_jAppProjectUITests
+@end
---
+++ Pods/Target Support Files/Pods-jAppProject/Pods-jAppProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jAppProject : NSObject
+@end
+@implementation PodsDummy_Pods_jAppProject
+@end
